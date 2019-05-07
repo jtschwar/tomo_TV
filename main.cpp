c3cadf907e35f5b0807b6d8de4393d2af5853e59
Jonathan Schwartz <jtschw@umich.edu>
2019-05-06 20:43:02 -0400

    First Commit.

--- main.cpp
+++ main.cpp
@@ -19,16 +19,6 @@ int beta_red = 095;
 int gamma_red;
 
 int main(int argc, const char * argv[]) {
-    Matrix3f A;
-    Vector3f b;
-    A << 1,2,3,  4,5,6,  7,8,10;
-    b << 3,3,4;
-    
-    cout << "Here is Matrix A" << A << endl;
-    cout << "Here is Vector b" << b << endl;
-    
-    Vector3f x = A.colPivHouseholderQr().solve(b);
-    cout << "The solution is: " << x << endl;
     
     return 0;
 }
