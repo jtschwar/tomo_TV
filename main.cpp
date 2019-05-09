b78b74891c5e08a15e218f743bfc4a3ad3ae440e
Hovden Group <hovdengroup@adnins-iMac.local>
2019-05-09 11:49:01 -0400

    Add ImageMagick as dependency.

--- main.cpp
+++ main.cpp
@@ -14,29 +14,18 @@
 using namespace Eigen;
 using namespace std;
 
-int Niter = 100;
-int ng = 20;
+int Niter = 25;
+//int ng = 20;
 int beta = 1.0;
 float beta_red = 0.95;
-int gamma_red = -5;
+//int gamma_red = -5;
 
 int main(int argc, const char * argv[]) {
     
-    Vector2d row, col, val;
-    row(0) = 0;
-    row(1) = 1;
-    col(0) = 0;
-    col(1) = 0;
-    val(0) = 10;
-    val(1) = 20;
-    SparseMatrix<double, RowMajor> sm(3,3);
     
-    for(int i = 0; i < 2; i++)
-    {
-        sm.insert(row(i), col(i)) = val(i);
-    }
+    SparseMatrix<float, RowMajor> A;
     
-    cout << sm << '\n';
+    
     
     return 0;
 }
