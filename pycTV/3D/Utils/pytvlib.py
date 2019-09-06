f2c7bb32b536f907cf4adf0ff2d3143ba8f7b445
Jonathan Schwartz <jtschw@umich.edu>
2019-09-06 15:31:14 -0400

    More efficient 3D - tv minimization.

--- pycTV/3D/Utils/pytvlib.py
+++ pycTV/3D/Utils/pytvlib.py
@@ -4,29 +4,24 @@ import time
 
 def tv_derivative(recon):
     r = np.lib.pad(recon, ((1, 1), (1, 1), (1, 1)), 'edge')
-    v1n = 3 * r - np.roll(r, 1, axis=0) - \
-                          np.roll(r, 1, axis=1) - np.roll(r, 1, axis=2) # noqa TODO reformat this
-    v1d = np.sqrt(1e-8 + (r - np.roll(r, 1, axis=0))**2 + (r -
-                  np.roll(r, 1, axis=1))**2 + (r - np.roll(r, 1, axis=2))**2) # noqa TODO reformat this
-
-    v2n = r - np.roll(r, -1, axis=0)
-    v2d = np.sqrt( 1e-8 + ( np.roll(r, -1, axis=0) - r )**2 +
+    v = 3 * r - np.roll(r, 1, axis=0) - \
+                          np.roll(r, 1, axis=1) - np.roll(r, 1, axis=2) / \
+        np.sqrt(1e-8 + (r - np.roll(r, 1, axis=0))**2 + (r -
+                  np.roll(r, 1, axis=1))**2 + (r - np.roll(r, 1, axis=2))**2)
+    v += r - np.roll(r, -1, axis=0) / \
+         np.sqrt( 1e-8 + ( np.roll(r, -1, axis=0) - r )**2 +
             ( np.roll(r, -1, axis=0) - np.roll(r, [-1,1], axis=(0,1)) )**2 +
-            ( np.roll(r, -1, axis=0) - np.roll(r, [-1,1], axis=(0,2)) )**2 ) # noqa TODO reformat this
-
-    v3n = r - np.roll(r, -1, axis=1)
-    v3d = np.sqrt( 1e-8 + 
+            ( np.roll(r, -1, axis=0) - np.roll(r, [-1,1], axis=(0,2)) )**2 )
+    v += r - np.roll(r, -1, axis=1) / \
+         np.sqrt( 1e-8 + 
                  (np.roll(r, -1, axis=1) - np.roll(r, [-1,1], axis=(1,0)))**2 +
                  (np.roll(r, -1, axis=1) - r)**2 + 
                  ( np.roll(r, -1, axis=1) - np.roll(r, [-1,1], axis=(1,2)))**2) 
-
-    v4n = r - np.roll(r, -1, axis=2)
-    v4d = np.sqrt(1e-8 + 
+    v += r - np.roll(r, -1, axis=2) / \
+         np.sqrt(1e-8 + 
                  (np.roll(r, -1, axis=2) - np.roll(r, [-1,1], axis=(2,0)))**2 +
                  (np.roll(r, -1, axis=2) - np.roll(r, [-1,1], axis=(2,1)))**2 +
                  (np.roll(r, -1, axis=2) - r)**2)
-
-    v = v1n / v1d + v2n / v2d + v3n / v3d + v4n / v4d
     v = v[1:-1, 1:-1, 1:-1]
     return v
 
